8d3e4840ec4c6d94a0ac6e0931e65bc2335feeb9
Aycan <aycan.biilge@gmail.com>
2024-06-27 08:17:35 -0400

    Implemented methods into MySqlShopping CartDao

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -1,4 +1,38 @@
 package org.yearup.data.mysql;
 
-public class MySqlShoppingCartDao {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+
+    @Autowired
+    public MySqlShoppingCartDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        return null;
+    }
+
+    @Override
+    public ShoppingCartItem add(int userId, ShoppingCartItem shoppingCartItem) {
+        return null;
+    }
+
+    @Override
+    public void update(int userId, ShoppingCartItem shoppingCartItem) {
+
+    }
+
+    @Override
+    public void delete(int userId) {
+
+    }
 }
