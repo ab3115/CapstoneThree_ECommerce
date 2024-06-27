987b9440d226da5f4843653667fa404588a0b065
Aycan <aycan.biilge@gmail.com>
2024-06-27 15:28:36 -0400

    added getByUserId method in ShoppingCartDao

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -3,10 +3,15 @@ package org.yearup.data.mysql;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.ShoppingCartItem;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Component
 public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
@@ -16,10 +21,47 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
         super(dataSource);
     }
 
+
     @Override
     public ShoppingCart getByUserId(int userId) {
-        return null;
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        String sql = "SELECT p.*, quantity " +
+                " FROM products AS p " +
+                " JOIN shopping_cart AS s " +
+                "   ON p.product_id = s.product_id " +
+                " WHERE user_id = ?";
+
+        try(
+                Connection connection = getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        )
+        {
+            preparedStatement.setInt(1, userId);
+
+            try(ResultSet row = preparedStatement.executeQuery();)
+            {
+                while(row.next())
+                {
+                    Product product = MySqlProductDao.mapRow(row);
+                    int quantity = row.getInt("quantity");
+                    ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
+                    shoppingCartItem.setProduct(product);
+                    shoppingCartItem.setQuantity(quantity);
+                    shoppingCart.add(shoppingCartItem);
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return shoppingCart;
     }
+
+
+
 
     @Override
     public ShoppingCartItem add(int userId, ShoppingCartItem shoppingCartItem) {
