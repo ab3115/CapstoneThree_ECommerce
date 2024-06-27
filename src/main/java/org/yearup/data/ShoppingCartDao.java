b521b2483eca0791ff3931f283a27233d81731ec
Aycan <aycan.biilge@gmail.com>
2024-06-26 23:49:06 -0400

    added additional methods for shappingcartDao interface

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,15 @@
 package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+    ShoppingCartItem add(int userId, ShoppingCartItem shoppingCartItem);
+
+    void update(int userId, ShoppingCartItem shoppingCartItem);
+
+    void delete(int userId);
 }
